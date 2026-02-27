657fbecd2abda28044ee30989845fc07095fb2c0
Javier Salinas <107282454+Salinas-Javier@users.noreply.github.com>
2026-02-27 11:00:54 -0300

    Update carga_datos.sql

--- scripts/carga_datos.sql
+++ scripts/carga_datos.sql
@@ -4,7 +4,7 @@ Carga masiva de los datos
 =============================================================
 */
 
-
+TRUNCATE TABLE bronze.crm_cust_info
 BULK INSERT bronze.crm_cust_info
 FROM 'C:\Users\Usuario\Documents\Data_engieenering\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH (
